6d300e9bc77f8aea87c2c9dfd31590c53566e3a1
morrhtechsolutions <joelubioka@gmail.com>
2022-07-05 19:00:52 +0100

    new change

--- index.php
+++ index.php
@@ -1,166 +1,172 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="https://kit.fontawesome.com/27ae433552.js" crossorigin="anonymous"></script>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://kit.fontawesome.com/27ae433552.js" crossorigin="anonymous"></script>
 </head>
+
 <body>
-<?php require "include/header1.php"; ?>
+        <?php require "include/header1.php"; ?>
 
-<!-- Slider Area -->
-<div class="slider">
-  <img src="images/slider.jpeg" height="70%" width="100%" alt="">
-</div>
-
-<!-- Archievment Area -->
-  <div>
-      <div class="Archievment_info">
-          <div class="Archievment_items">
-          
-          <i class="fa-solid fa-chart-column fa-2x"></i>
-                    <p class="item-text">9 Years Of Experience And Still Counting </p>
-            
-          </div>
-
-          <div class="Archievment_items">
-          <i class="fa-solid fa-people-group fa-2x"></i>
-                  <div>
-                     <p>300+ Trained Chartered Accountants</p>
-                  </div>
-          </div>
-
-          <div class="Archievment_items">
-          <i class="fa-solid fa-star fa-2x"></i>
-                  <div>
-                     <p>Awards Won And Still Counting</p>
-                  </div>
-          </div>
-      </div>   
-  </div>
-
-  <!-- Courses -->
-
-  <div>
-      <h2 class="h2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Courses</h2>
-
-      <div class="Courses">
-          <div class="Courses_info">
-            <a href="courses.php">
-              <div class="Courses_item1">
-                       <p class="Course_text0">ALL</p>
-              </div>
-              </a>
-
-              <a href="partners.php">
-          <div class="Courses_item2">
-          <p class="Course_text">PARTNERS</p>
-          </div>
-          </a>
+        <!-- Slider Area -->
+        <div class="slider">
+                <img src="images/slider.jpeg" height="70%" width="100%" alt="">
         </div>
 
-      </div>
+        <!-- Archievment Area -->
+        <div>
+                <div class="Archievment_info">
+                        <div class="Archievment_items">
 
-      <div class="Courses">
-        <div class="Courses_info">
-            <a href="certification.php">
-                <div class="Courses_item3">
-                    <div>
-                      <p class="Course_text1">CERTIFICATION</p><small>5 Courses</small>
-                    </div>
-                </div>
-            </a>
+                                <i class="fa-solid fa-chart-column fa-2x"></i>
+                                <p class="item-text">13 Years Of Experience And Still Counting </p>
 
-            <a href="IT_training.php">
-              <div class="Courses_item4">
-                  <div>
-                    <p class="Course_text1">I.T TRAINING</p><small>3 Courses</small>
-                  </div>
+                        </div>
+
+                        <div class="Archievment_items">
+                                <i class="fa-solid fa-people-group fa-2x"></i>
+                                <div>
+                                        <p>300+ Trained Chartered Accountants</p>
+                                </div>
+                        </div>
+
+                        <div class="Archievment_items">
+                                <i class="fa-solid fa-star fa-2x"></i>
+                                <div>
+                                        <p>Awards Won And Still Counting</p>
+                                </div>
+                        </div>
                 </div>
-            </a>
         </div>
-      </div>
 
-      <div class="Courses">
-          <div class="Courses_info">
-             <a href="foreign_exams.php">
-               <div class="Courses_item5">
-                  <div>
-                      <p class="Course_text1">FOREIGN</p><small>4 Courses</small>
-                  </div>
-               </div>
-             </a>
-          </div>
-      </div>
+        <!-- Courses -->
 
-  </div>
-  <!-- Courses Ends -->
+        <div>
+                <h2 class="h2">
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Courses</h2>
+
+                <div class="Courses">
+                        <div class="Courses_info">
+                                <a href="courses.php">
+                                        <div class="Courses_item1">
+                                                <p class="Course_text0">ALL</p>
+                                        </div>
+                                </a>
+
+                                <a href="partners.php">
+                                        <div class="Courses_item2">
+                                                <p class="Course_text">PARTNERS</p>
+                                        </div>
+                                </a>
+                        </div>
+
+                </div>
+
+                <div class="Courses">
+                        <div class="Courses_info">
+                                <a href="certification.php">
+                                        <div class="Courses_item3">
+                                                <div>
+                                                        <p class="Course_text1">CERTIFICATION</p><small>5
+                                                                Courses</small>
+                                                </div>
+                                        </div>
+                                </a>
+
+                                <a href="IT_training.php">
+                                        <div class="Courses_item4">
+                                                <div>
+                                                        <p class="Course_text1">I.T TRAINING</p><small>3 Courses</small>
+                                                </div>
+                                        </div>
+                                </a>
+                        </div>
+                </div>
+
+                <div class="Courses">
+                        <div class="Courses_info">
+                                <a href="foreign_exams.php">
+                                        <div class="Courses_item5">
+                                                <div>
+                                                        <p class="Course_text1">FOREIGN</p><small>4 Courses</small>
+                                                </div>
+                                        </div>
+                                </a>
+                        </div>
+                </div>
+
+        </div>
+        <!-- Courses Ends -->
 
 
 
 
-  <!-- Why Choose Us -->
-  <div class="General_div">
+        <!-- Why Choose Us -->
+        <div class="General_div">
 
-      <div class="Why_Treasurehall_image_area">
-          <div class="image_and_text">
-            <!-- the styling was done in the css style sheet -->
-          </div>
-      </div>
-  
-  </div>
-  <!-- End of why choose us section -->
+                <div class="Why_Treasurehall_image_area">
+                        <div class="image_and_text">
+                                <!-- the styling was done in the css style sheet -->
+                        </div>
+                </div>
+
+        </div>
+        <!-- End of why choose us section -->
 
 
-  <!-- Reviews Area Starts -->
-  <div>
-    .
-  </div>
-  <div class="Review_heading">
-  <div  class="head_padding">
-    fkj
-  </div>
-      <h2>WHAT OUR STUDENTS HAVE TO SAY...</h2>
-      <div>
-      <div class="Reviews">
-          <div class="Review_content">
-            
-          </div>
+        <!-- Reviews Area Starts -->
+        <div>
+                .
+        </div>
+        <div class="Review_heading">
+                <div class="head_padding">
+                        fkj
+                </div>
+                <h2>WHAT OUR STUDENTS HAVE TO SAY...</h2>
+                <div>
+                        <div class="Reviews">
+                                <div class="Review_content">
 
-          <div class="Review_content1">
-           
-          </div>
+                                </div>
 
-          <div class="Review_content2">
-             
-          </div>
-      </div>   
-  </div>
+                                <div class="Review_content1">
 
-  <!-- Our Team -->
-  <div class="fees"></div>
-     <div class="Our_team_div">
-     .. <br><br><br>.
-          <div class="image_and_writeup">
-             <img src="images/review1.jpeg" alt="">
-             <div>
-               <p class="text">We have a team of Thoroughbred proffesionals readily available to you for Consultation and Counseling services.</p>
-             </div>
-             
-          </div>           
-        ...
-     </div>
-  </div>
-</div>
-<!-- Our Team Ends -->
+                                </div>
 
-<!-- footer -->
-<div>.</div>
-<?php require "include/footer.php"; ?>
+                                <div class="Review_content2">
 
-<script src="javascript/script.js"></script>
+                                </div>
+                        </div>
+                </div>
+
+                <!-- Our Team -->
+                <div class="fees"></div>
+                <div class="Our_team_div">
+                        .. <br><br><br>.
+                        <div class="image_and_writeup">
+                                <img src="images/review1.jpeg" alt="">
+                                <div>
+                                        <p class="text">We have a team of Thoroughbred proffesionals readily available
+                                                to you for Consultation and Counseling services.</p>
+                                </div>
+
+                        </div>
+                        ...
+                </div>
+        </div>
+        </div>
+        <!-- Our Team Ends -->
+
+        <!-- footer -->
+        <div>.</div>
+        <?php require "include/footer.php"; ?>
+
+        <script src="javascript/script.js"></script>
 </body>
+
 </html>
--- services.php
+++ services.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="https://kit.fontawesome.com/27ae433552.js" crossorigin="anonymous"></script>
-    <title>Document</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://kit.fontawesome.com/27ae433552.js" crossorigin="anonymous"></script>
+        <title>Document</title>
 </head>
+
 <body>
-<div class="header_section"><?php require "include/header1.php"; ?></div>
-<!-- GMAT Slider -->
-<div class="gmat_slider_div">
-    <img src="images/services/service(1).jpeg" alt="">
-</div>
-<img src="images/services/stage(2).jpeg" width="100%" alt="">
+        <div class="header_section"><?php require "include/header1.php"; ?></div>
+        <!-- GMAT Slider -->
+        <div class="gmat_slider_div">
+                <img src="images/services/services.png" alt="">
+        </div>
+        <img src="images/services/stage(2).jpeg" width="100%" alt="">
 
-<img src="images/services/stage(3).png" width="100%" alt=""><br><br><br><br><br><br>
+        <img src="images/services/skill.png" width="100%" alt=""><br><br><br><br><br><br>
 
-<div class="fees">
-        <!-- The image is in the CSS -->
-        <img src="images/services/stage(4).jpeg" alt="">
-    </div>
+        <div class="fees">
+                <!-- The image is in the CSS -->
+                <img src="images/services/sourcing.png" alt="">
+        </div>
 
-<img src="images/services/stage(5).jpeg" width="100%" alt="">
-   <div class="footer_section"> <?php require "include/footer.php"; ?> </div>
+        <img src="images/services/stage(5).jpeg" width="100%" alt="">
+        <div class="footer_section"> <?php require "include/footer.php"; ?> </div>
 
 
-    <script src="javascript/script.js"></script>
+        <script src="javascript/script.js"></script>
 
 </body>
+
 </html>
